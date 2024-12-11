1f16c0b2d317ef37183ea5108dd8ae460f005e05
Marcelo Alexandre Barrionuevo <mabarril@gmail.com>
2024-12-11 00:21:43 -0300

    adiciona validação global e DTO para criação de usuário com regras de validação

--- src/usuario/dto/criaUsuario.dto.ts
+++ src/usuario/dto/criaUsuario.dto.ts
@@ -1,5 +1,13 @@
-export class CriaUsuarioDto {
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
+export class CriaUsuarioDTO {
+
+    @IsNotEmpty({ message: 'O campo nome é obrigatório' })
     nome: string;
+
+    @IsEmail({}, { message: 'O campo email é inválido' })
     email: string;
+
+    @MinLength(6, { message: 'O campo senha deve ter no mínimo 6 caracteres' })
     senha: string;
 }
--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
-import { CriaUsuarioDto } from "./dto/criaUsuario.dto";
+import { CriaUsuarioDTO } from "./dto/criaUsuario.dto";
 
 @Controller('/usuarios') 
     export class UsuarioController {
         constructor(private usuarioRepository: UsuarioRepository) {}
         
         @Post()
-        async criaUsuario(@Body() dadosDoUsuario : CriaUsuarioDto) {  
+        async criaUsuario(@Body() dadosDoUsuario : CriaUsuarioDTO) {  
             this.usuarioRepository.salvar(dadosDoUsuario); 
             return dadosDoUsuario;
         }
